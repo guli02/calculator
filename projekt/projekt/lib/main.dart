f4ce03a9ebeaa8a51ccb6672eccea98ab7750434
Elebesova Gulzat <88238675+guli02@users.noreply.github.com>
2023-03-28 09:41:03 +0600

    guli02
    calculator

---
+++ projekt/projekt/lib/main.dart
@@ -0,0 +1,135 @@
+//import 'package:calculator_tutorial/buttons.dart';
+import 'package:flutter/material.dart';
+import 'button.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var userQuestion = '';
+  var userAnswer = '';
+
+  final myTextStyle = TextStyle(fontSize: 30, color: Colors.deepPurple[900]);
+
+  final List<String> buttons = [
+    'C',
+    'Del',
+    '%',
+    '/',
+    '9',
+    '8',
+    '7',
+    'x',
+    '6',
+    '5',
+    '4',
+    '-',
+    '3',
+    '2',
+    '1',
+    '=',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[100],
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  SizedBox(height: 50,),
+                  Container(
+                      padding: EdgeInsets.all(20),
+                      alignment: Alignment.centerLeft,
+                      child: Text(userQuestion, style: TextStyle(fontSize: 20),),
+                      ),
+                  Container(
+                      padding: EdgeInsets.all(20),
+                      alignment: Alignment.centerRight,
+                      child: Text(userAnswer, style: TextStyle(fontSize: 20),),
+                      ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              child: GridView.builder(
+                  itemCount: buttons.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4
+                      ),
+                  itemBuilder: (BuildContext context, int index) {
+                    if (index == 0) {
+                      return MyButton(
+                        buttonTapped: (){
+                          setState(() {
+                            userQuestion = '';
+                          });
+                        },
+                        buttonText: buttons[index],
+                        color: Colors.green,
+                        textColor: Colors.white,
+                      );
+                    } else if (index == 1) {
+                      return MyButton(
+                        buttonTapped: (){
+                          setState(() {
+                            userQuestion = userQuestion.substring(0, userQuestion.length-1);
+                          });
+                        },
+                        buttonText: buttons[index],
+                        color: Colors.red,
+                        textColor: Colors.white,
+                      );
+                    } else {
+                      return MyButton(
+                        buttonTapped: (){
+                          setState(() {
+                            userQuestion += buttons[index];
+                          });
+                        },
+                        buttonText: buttons[index],
+                        color: isOperator(buttons[index])
+                            ? Colors.deepPurple
+                            : Colors.deepPurple[50],
+                        textColor: isOperator(buttons[index])
+                            ? Colors.white
+                            : Colors.deepPurple,
+                      );
+                    }
+                  }
+                  ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  bool isOperator(String x) {
+    if (x == '%' || x == '/' || x == 'x' || x == '-' || x == '+' || x == '=') {
+      return true;
+    }
+    return false;
+  }
+}
